5a75327ff1385e10496b0ec403b825249ebb6fe6
gmarinozzi <gmarinozzi@gmail.com>
2016-09-21 19:32:56 -0500

    banner, tracks y regions

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,17 @@
 //= require_tree .
+
+$(document).ready(function(){
+  $(".slider").slick({
+    dots: false,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 3
+  });
+
+  $('.js-accordion-trigger').bind('click', function(e){
+    jQuery(this).parent().find('p').slideToggle('fast');
+    jQuery(this).parent().toggleClass('is-expanded');
+    e.preventDefault();
+  });
+
+});
